7e4efa366b4796538fec78846ba5a693acaeae20
espen <espenk95@gmail.com>
2018-10-22 20:25:38 +0200

    Use logging handlers instead of basicConfig.

--- client.py
+++ client.py
@@ -13,11 +13,22 @@ import datetime
 URL = 'http://104.248.47.74/dkrest/'
 
 
-# create logger
-logging.basicConfig(filename='debug.log',
-                    level=logging.DEBUG,
-                    format='%(asctime)s; %(name)s; %(levelname)s; %(message)s')
 log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s; %(name)s; %(levelname)s; %(message)s')
+
+console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('debug.log')
+
+console_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
+
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+log.addHandler(console_handler)
+log.addHandler(file_handler)
 
 
 def logger(func):

c99616dbb9a555835218dbe14b18080dc77301d4
espen <espenk95@gmail.com>
2018-10-22 18:01:13 +0200

    Add fetch() that uses aiohttp ClientSession to make a request and return the response as dict or string.

--- client.py
+++ client.py
@@ -3,6 +3,22 @@ import aiohttp
 import json
 
 
+async def fetch(url: str, params: dict = None, data = None, method: str = 'GET') -> [dict, str, None]:
+    """Make a request with the provided method, url and parameters and return the content
+
+    :param url: The url to request data from.
+    :param params: A dictionary of key value pairs to be sent as parameters.
+    :param data: Data to be sent in the body of the request.
+    :param method: The HTTP method to use for the request.
+    :return: The contents of the response.
+    """
+    async with session.request(method=method, url=url, params=params, data=data) as response:
+        if response.content_type == 'application/json':
+            return await response.json()
+        else:
+            return await response.text()
+
+
 async def main():
 
     await session.close()
